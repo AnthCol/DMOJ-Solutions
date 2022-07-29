3807cd2f17917d9c106fcede592c965fa736e44a
AnthCol <82617912+AnthCol@users.noreply.github.com>
2022-07-29 00:21:07 -0400

    3pp

---
+++ valentinedDay19j2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int total = 0; 
+    scanf("%d", &total); 
+    getchar(); 
+    int count = 0; 
+    int r; 
+    int g; 
+    int b; 
+    
+    
+    for (int i = 0; i < total; i++){
+        scanf("%d %d %d", &r, &g, &b); 
+        getchar();
+        if (r <= 255 && r >= 240){
+            if (g >= 0 && g <= 200){
+                if (b >= 95 && b <= 220){
+                    count++; 
+                }
+            }
+        }
+        
+    }
+    
+    printf("%d\n", count); 
+    
+    return 0;
+}

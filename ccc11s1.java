875a7d835ed27509501d39b16dfd3da381a01652
AnthCol <82617912+AnthCol@users.noreply.github.com>
2022-07-29 00:47:55 -0400

    5pp

--- ccc11s1.java
+++ ccc11s1.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
-public class HelloWorld {
+
+public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in); 
